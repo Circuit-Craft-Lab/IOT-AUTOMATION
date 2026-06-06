a28097e42d8df7e6e680c63e8850ca65573f5c51
Circuit-Craft-Lab <sandeep14.nain@gmail.com>
2026-06-06 15:49:25 +0530

    Home Automation.ino

--- Home Automation/Home Automation.ino
+++ Home Automation/Home Automation.ino
@@ -1,1 +1,44 @@
+#include <IRremote.hpp>
+
+int IR_PIN = 2;
+int relay1 = 11;
+int relay2 = 12;
+
+bool state1 = LOW;
+bool state2 = LOW;
+
+void setup() {
+Serial.begin(9600);
+pinMode(relay1, OUTPUT);
+pinMode(relay2, OUTPUT);
+
+IrReceiver.begin(IR_PIN, ENABLE_LED_FEEDBACK);
+}
+
+void loop() {
+if (IrReceiver.decode()) {
+
+unsigned long code = IrReceiver.decodedIRData.decodedRawData;
+Serial.println(code, HEX);
+
+// Button 1
+if (code == 0xFD02BF00) {
+state1 = !state1;
+digitalWrite(relay1, state1);
+}
+// Button 2
+if (code == 0xFC03BF00) {
+state2 = !state2;
+digitalWrite(relay2, state2);
+}
+
+IrReceiver.resume();
+}
+}
+
+
+
+
+
+
 
